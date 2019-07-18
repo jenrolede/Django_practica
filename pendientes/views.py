06c631e79fc8a71f9aebf2fe0a663cb9c3fcb02a
jenrolede <sol.bencas@gmail.com>
2019-07-19 00:26:46 +0200

    desarrollo de la clase

--- pendientes/views.py
+++ pendientes/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from pendientes.models import Tarea# importamos la clase tarea de models 
+from random import randint
 # Create your views here.
 
-def index(request):
-    saludo = "Hola, Mundo! Esta es la raiz /"
-    return HttpResponse(saludo) #retornamos el saludo
+def index(request): 
+    listita = Tarea.objects.all() #consultamos la BDy guardamos todos
+                                # los registros de la tabla Tarea
+                                # como objetos y guardamos en listita
+    persona = {
+        "nombre":"Jenny",
+        "Edad":20,
+        "Hobbies":["Bailar", "Dibujar", "Cantar"],
+        "lista_tareas": listita, # agregamos la clave lista_tareas
+                                 # al diccionario de contenido con la
+                                 #
+    }
+    return render(request, "inicio.html", persona)
+
+def tarea(request):
+    return HttpResponse("hola soy la vista de tarea")
+
+def respuesta(request):
+    return HttpResponse("hola soy la vista de respuesta")
+
+#def aleatorio(request):
+    #numero =str(randint(500,999))
